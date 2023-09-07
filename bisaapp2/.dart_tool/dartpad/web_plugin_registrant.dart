26dc43b7f86b1687d7e44588939ed7140afef216
ninjaVr2 <136304943+ninjaVr2@users.noreply.github.com>
2023-09-07 16:19:01 +0000

    sub_app
    sub_app

---
+++ bisaapp2/.dart_tool/dartpad/web_plugin_registrant.dart
@@ -0,0 +1,26 @@
+// Flutter web plugin registrant file.
+//
+// Generated file. Do not edit.
+//
+
+// @dart = 2.13
+// ignore_for_file: type=lint
+
+import 'package:audioplayers_web/audioplayers_web.dart';
+import 'package:flutter_native_splash/flutter_native_splash_web.dart';
+import 'package:image_picker_for_web/image_picker_for_web.dart';
+import 'package:record_web/record_web.dart';
+import 'package:shared_preferences_web/shared_preferences_web.dart';
+import 'package:url_launcher_web/url_launcher_web.dart';
+import 'package:flutter_web_plugins/flutter_web_plugins.dart';
+
+void registerPlugins([final Registrar? pluginRegistrar]) {
+  final Registrar registrar = pluginRegistrar ?? webPluginRegistrar;
+  AudioplayersPlugin.registerWith(registrar);
+  FlutterNativeSplashWeb.registerWith(registrar);
+  ImagePickerPlugin.registerWith(registrar);
+  RecordPluginWeb.registerWith(registrar);
+  SharedPreferencesPlugin.registerWith(registrar);
+  UrlLauncherPlugin.registerWith(registrar);
+  registrar.registerMessageHandler();
+}
